e0d45fc9d0825e259390bb3afb183d144a4a440f
sultaninejad <tayyebsultaninejad@gmail.com>
2024-07-13 19:01:40 +0330

    the first version of homescreen completed- animation two icon

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -8,6 +8,8 @@ import 'package:language_app/core/services/auth_service.dart';
 import 'package:language_app/features/home/presentation/pages/login_screen.dart';
 import 'package:language_app/features/home/presentation/pages/register_screen.dart';
 
+import 'package:lottie/lottie.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -20,100 +22,152 @@ class HomeScreen extends StatelessWidget {
     final currentUser = authService.currentUser;
 
     return Scaffold(
-      appBar: _buildAppBar(context),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('bg1.webp'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child:  currentUser != null 
-           ? Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        appBar: _buildAppBar(context),
+        body: Stack(
           children: [
-            DailyLessonWidget(lesson: currentLesson),
-            const SizedBox(height: 20),
-            const Spacer(),
-            const BottomNavBar(),
-          ],
-        ),
-      )
-      : Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Please log in to access your lessons and flashcards.'),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()),
-                      );
-                    },
-                    child: Text('Login'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => RegisterScreen()),
-                      );
-                    },
-                    child: Text('Register'),
-                  ),
-                ],
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('bg1.webp'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-      ),
-    );
+            Center(
+              child: currentUser != null
+                  ? Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          DailyLessonWidget(lesson: currentLesson),
+                          const SizedBox(height: 20),
+                          const Spacer(),
+                          const BottomNavBar(),
+                        ],
+                      ),
+                    )
+                  : Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Lottie.asset(
+                          'flesh.json',
+                          width: 200,
+                          height: 200,
+                        ),
+                        const SizedBox(height: 20),
+                        Card(
+                          color: Colors.lightBlue[50],
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          elevation: 10,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Welcome to Language Learning App',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 6, 101, 124),
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 40),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 30, vertical: 15),
+                                shape: CircleBorder(),
+                                backgroundColor: Colors.blueAccent,
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => RegisterScreen()),
+                                );
+                              },
+                              child: Icon(Icons.person_add,
+                                  size: 40, color: Colors.white),
+                            ),
+                            SizedBox(width: 40),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 30, vertical: 15),
+                                shape: CircleBorder(),
+                                backgroundColor: Colors.greenAccent,
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => LoginScreen()),
+                                );
+                              },
+                              child: Icon(Icons.login,
+                                  size: 40, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+            )
+          ],
+        ));
   }
 }
 
 PreferredSizeWidget? _buildAppBar(BuildContext context) {
-    final authService = Provider.of<AuthService>(context);
+  final authService = Provider.of<AuthService>(context);
 
-    if (authService.currentUser != null) {
-      return AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Hello, ${authService.currentUser!.username}'),
-            IconButton(
-              icon: Icon(Icons.logout),
-              onPressed: () {
-                authService.logout();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomeScreen()),
-                );
-              },
+  if (authService.currentUser != null) {
+    return AppBar(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('Hello, ${authService.currentUser!.username}'),
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              authService.logout();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => HomeScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+      backgroundColor: Colors.lightBlue[300],
+      elevation: 10,
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.blueAccent,
+              Colors.lightBlue,
+              Colors.lightBlueAccent
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 8,
+              offset: Offset(2, 4),
             ),
           ],
         ),
-        backgroundColor: Colors.lightBlue[300],
-        elevation: 10,
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.blueAccent, Colors.lightBlue, Colors.lightBlueAccent],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 8,
-                offset: Offset(2, 4),
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-    return null;
+      ),
+    );
   }
+  return null;
+}
