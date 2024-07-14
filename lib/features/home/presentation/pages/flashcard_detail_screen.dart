8959f4a53553688786974cbdd6292af9fef078ee
sultaninejad <tayyebsultaninejad@gmail.com>
2024-07-14 15:14:17 +0330

    separate flashcard and details, add deck title

---
+++ lib/features/home/presentation/pages/flashcard_detail_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:language_app/core/models/flashcard.dart';
+
+
+
+class FlashcardDetailScreen extends StatefulWidget {
+  final String deckName;
+  final Flashcard flashcard;
+
+  FlashcardDetailScreen({required this.deckName, required this.flashcard});
+
+  @override
+  _FlashcardDetailScreenState createState() => _FlashcardDetailScreenState();
+}
+
+class _FlashcardDetailScreenState extends State<FlashcardDetailScreen> {
+  bool showAnswer = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.deckName),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/bg4.webp'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    showAnswer = !showAnswer;
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.all(24.0),
+                  color: Colors.blueAccent,
+                  child: Text(
+                    showAnswer ? widget.flashcard.answer : widget.flashcard.question,
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Back to Deck'),
+              ),
+            ],
+                    ),
+          ),
+      ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/pages/flashcard_screen.dart
+++ lib/features/home/presentation/pages/flashcard_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+
 import 'package:provider/provider.dart';
 import 'package:language_app/core/services/auth_service.dart';
-import 'package:language_app/core/models/flashcard.dart';
 import '../../widgets/add_flashcard_widget.dart';
 import '../../widgets/create_deck_widget.dart';
+import 'package:language_app/features/home/presentation/pages/flashcard_detail_screen.dart';
 
 class FlashcardScreen extends StatefulWidget {
   @override
@@ -34,14 +35,20 @@ class _FlashcardScreenState extends State<FlashcardScreen> {
   Widget build(BuildContext context) {
     final decks = authService.getUserDecks();
 
-    // log user
-    print('flashcard screen:');
-    // Log the decks
-    print('Decks: ${decks.keys.toList()}');
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flashcards'),
-      ),
+      appBar: selectedDeck != null
+          ? AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  setState(() {
+                    selectedDeck = null;
+                  });
+                },
+              ),
+              title: Text('${selectedDeck!} Deck'),
+            )
+          : null,
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
@@ -50,132 +57,93 @@ class _FlashcardScreenState extends State<FlashcardScreen> {
           ),
         ),
         child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: selectedDeck == null
-            ? Column(
-                children: [
-                  Expanded(
-                    child: decks.isEmpty
-                        ? Center(child: Text('No decks available.'))
-                        : ListView.builder(
-                            itemCount: decks.keys.length,
-                            itemBuilder: (context, index) {
-                              String deckName = decks.keys.elementAt(index);
-                              return Card(
-                                margin: EdgeInsets.symmetric(vertical: 10),
-                                child: ListTile(
-                                  title: Text(deckName),
-                                  onTap: () {
-                                    setState(() {
-                                      selectedDeck = deckName;
-                                    });
-                                  },
-                                ),
-                              );
-                            },
+          padding: const EdgeInsets.all(16.0),
+          child: selectedDeck == null
+              ? Column(
+                  children: [
+                     Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Card(
+                        color: Colors.lightBlue[50],
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        elevation: 10,
+                        child: const Padding(
+                          padding:  EdgeInsets.all(16.0),
+                          child: Text(
+                            'Here is your Decks',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                  ),
-                  CreateDeckWidget(onCreateDeck: _createDeck),
-                ],
-              )
-            : Column(
-                children: [
-                  Expanded(
-                    child: decks[selectedDeck]!.isEmpty
-                        ? Center(child: Text('No flashcards available.'))
-                        : ListView.builder(
-                            itemCount: decks[selectedDeck]!.length,
-                            itemBuilder: (context, index) {
-                              var flashcard = decks[selectedDeck]![index];
-                              return Card(
-                                margin: EdgeInsets.symmetric(vertical: 10),
-                                child: ListTile(
-                                  title: Text(flashcard.question),
-                                   onTap: () {
-                                    
-                                       Navigator.push(
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        child: decks.isEmpty
+                            ? Center(child: Text('No decks available.'))
+                            : Stack(
+                                children: [
+                                  ListView.builder(
+                                    itemCount: decks.keys.length,
+                                    itemBuilder: (context, index) {
+                                      String deckName =
+                                          decks.keys.elementAt(index);
+                                      return Card(
+                                        margin:
+                                            EdgeInsets.symmetric(vertical: 10),
+                                        child: ListTile(
+                                          title: Text(deckName),
+                                          onTap: () {
+                                            setState(() {
+                                              selectedDeck = deckName;
+                                            });
+                                          },
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ],
+                              )),
+                    CreateDeckWidget(onCreateDeck: _createDeck),
+                  ],
+                )
+              : Column(
+                  children: [
+                    Expanded(
+                      child: decks[selectedDeck]!.isEmpty
+                          ? Center(child: Text('No flashcards available.'))
+                          : ListView.builder(
+                              itemCount: decks[selectedDeck]!.length,
+                              itemBuilder: (context, index) {
+                                var flashcard = decks[selectedDeck]![index];
+                                return Card(
+                                  margin: EdgeInsets.symmetric(vertical: 10),
+                                  child: ListTile(
+                                    title: Text(flashcard.question),
+                                    onTap: () {
+                                      Navigator.push(
                                         context,
                                         MaterialPageRoute(
-                                          builder: (context) => FlashcardDetailScreen(
+                                          builder: (context) =>
+                                              FlashcardDetailScreen(
                                             deckName: selectedDeck!,
                                             flashcard: flashcard,
                                           ),
                                         ),
                                       );
-                                   
-                                  },
-                                ),
-                              );
-                            },
-                          ),
-                  ),
-                  AddFlashcardWidget(deckName: selectedDeck! , onAddFlashcard: _addFlashcard),
-                ],
-              ),
-      ),
-      ),
-    );
-  }
-}
-
-class FlashcardDetailScreen extends StatefulWidget {
-  final String deckName;
-  final Flashcard flashcard;
-
-  FlashcardDetailScreen({required this.deckName, required this.flashcard});
-
-  @override
-  _FlashcardDetailScreenState createState() => _FlashcardDetailScreenState();
-}
-
-class _FlashcardDetailScreenState extends State<FlashcardDetailScreen> {
-  bool showAnswer = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.deckName),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/bg4.webp'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Center(
-            child: Column(
-              children: [
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    showAnswer = !showAnswer;
-                  });
-                },
-                child: Container(
-                  padding: EdgeInsets.all(24.0),
-                  color: Colors.blueAccent,
-                  child: Text(
-                    showAnswer ? widget.flashcard.answer : widget.flashcard.question,
-                    style: TextStyle(fontSize: 24, color: Colors.white),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('Back to Deck'),
-              ),
-            ],
+                                    },
+                                  ),
+                                );
+                              },
+                            ),
                     ),
-          ),
-      ),
+                    AddFlashcardWidget(
+                        deckName: selectedDeck!, onAddFlashcard: _addFlashcard),
+                  ],
+                ),
+        ),
       ),
     );
   }
